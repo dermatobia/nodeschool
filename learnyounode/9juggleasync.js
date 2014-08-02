5e8f7d97a06ca74f8db05da46f9771e96129e94a
dermatobia <webblewebble@gmail.com>
2014-08-02 00:55:09 -0400

    ex 9

---
+++ learnyounode/9juggleasync.js
@@ -0,0 +1,68 @@
+var http = require('http');
+var urls = [];
+var obj = {};
+var counter = 0;
+var maxLength = process.argv.length-2
+
+for (i = 0; i < maxLength; i++ ){
+  urls[i] = process.argv[i+2];
+  url = urls[i];
+  obj[url] = '';
+
+  (function(url){
+    http.get(url, function(resp){
+      obj[url] = ''
+      resp.on("data", function(data){
+        obj[url] += data;
+      })
+      resp.on("end", function(){
+        counter++;
+        if (counter == maxLength){
+          print();
+        }
+        
+      })
+    });  
+  })(url);
+  
+};
+
+function print(){
+  for (i in urls){
+    console.log(obj[urls[i]])
+  }
+}
+
+
+
+// -------------------
+//  Official Solution
+// -------------------
+
+// var http = require('http')
+// var bl = require('bl')
+// var results = []
+// var count = 0
+
+// function printResults () {
+//   for (var i = 0; i < 3; i++)
+//     console.log(results[i])
+// }
+
+// function httpGet (index) {
+//   http.get(process.argv[2 + index], function (response) {
+//     response.pipe(bl(function (err, data) {
+//       if (err)
+//         return console.error(err)
+
+//       results[index] = data.toString()
+//       count++
+
+//       if (count == 3) // yay! we are the last one!
+//         printResults()
+//     }))
+//   })
+// }
+
+// for (var i = 0; i < 3; i++)
+//   httpGet(i)
